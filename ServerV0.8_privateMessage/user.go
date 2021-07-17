27caa53912f3b21b81ea3827ffa84ce08c59b2d3
lilin7 <36003947+lilin7@users.noreply.github.com>
2021-07-17 00:57:04 -0400

    minor: formating

--- ServerV0.8_privateMessage/user.go
+++ ServerV0.8_privateMessage/user.go
@@ -40,7 +40,7 @@ func (this *User) Online()  {
 	this.server.mapLock.Unlock()
 
 	//broadcast to user a success msg
-	this.server.BroadCast(this, " This user is online!")
+	this.server.BroadCast(this, "This user is online!")
 }
 
 func (this *User) Offline()  {
@@ -50,7 +50,7 @@ func (this *User) Offline()  {
 	this.server.mapLock.Unlock()
 
 	//broadcast to user a success msg
-	this.server.BroadCast(this, " This user is offline.")
+	this.server.BroadCast(this, "This user is offline.")
 }
 
 func (this *User) SendMessage(msg string) {
